35b9540d72994583f0c52583c813637591e1545a
nnzzcc <870391757@qq.com>
2020-02-04 02:51:43 +0800

    frist

---
+++ web/locales/zh-CN/resources.js
@@ -0,0 +1,99 @@
+window.webResources = {
+  "app": "SuperMap iClient 9D(2019)",
+  "nav": {
+    "home": {
+      "_name": "首页"
+    },
+    "overview": {
+      "_name": "概览",
+      "functions": "主要功能服务",
+      "compatibility": "浏览器兼容性",
+      "thirdpatry": "第三方插件",
+      "license": "许可协议",
+      "howToChoose": "如何选择"
+    },
+    "leaflet": {
+      "_name": "for Leaflet"
+    },
+    "openlayers": {
+      "_name": "for OpenLayers"
+    },
+    "mapboxgl": {
+      "_name": "for MapboxGL"
+    },
+    "classic": {
+      "_name": "Classic"
+    },
+    "3dwebgl": {
+      "_name": "3D-WebGL"
+    },
+    "product": {
+      "_name": "产品",
+      "introduce": "产品介绍",
+      "download": "下载",
+      "guide": "开发指南",
+      "modules": "模块说明",
+      "newAttributes": "新特性",
+      "dependence": "相关依赖",
+      "license": "许可说明",
+      "features": "技术特点"
+    },
+    "guide": {
+      "_name": "开发指南",
+      "beginning": "开发入门",
+      "update": "iClient 8C 升级",
+      "ie8": "兼容IE8",
+      "ready":"准备",
+      "multiProjection":"为地图设置投影",
+      "createMap":"创建一幅地图",
+      "vectorTile":"使用矢量瓦片",
+      "addControl":"添加控件",
+      "drawSymbolsAndGraphs":"绘制符号与图形",
+      "areaAndDistanceMeasurement":"面积和距离测量",
+      "queryMapInformation":"地物查询",
+      "editMapInformation":"地物编辑",
+      "thematicMap":"专题图",
+      "spatialAnalyst":"空间分析",
+      "networkAnalysis":"网络分析",
+      "clientComputing":"客户端计算",
+      "addressMatch":"地址匹配",
+      "bigDataAnalysis":"大数据分析",
+      "dataVisualization":"数据可视化",
+      "currentTimeVisualization":"数据流",
+      "ic9TrafficTransferAnalystService":"交通换乘分析",
+      "leafletDynamicPlot": "动态标绘"
+
+      
+    },
+    
+    "more": {
+      "_name": "更多>>",
+      "version": {
+        "_name": "历史版本"
+      },
+      "get": {
+        "_name": "下载",
+        "stable": "9.1.2",
+        "releases": "Releases",
+        "source": "源码",
+        "npm": "npm",
+        "oldVersions": "历史版本"
+      },
+      "language": {
+        "_name": "语言"
+      },
+      "faq": {
+        "_name": "FAQ"
+      }
+    },
+    "api": {
+      "_name": "API",
+      "doc": "文档",
+      "plotDoc": "文档 - 动态标绘"
+    },
+    "gallery": {
+      "_name": "Gallery"
+    },
+    "soon": "敬请期待"
+  }
+}
