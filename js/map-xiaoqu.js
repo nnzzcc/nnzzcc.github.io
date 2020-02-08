5e86ef9bf412b07d8c483e31d52bf01b8c7951df
nnzzcc <870391757@qq.com>
2020-02-08 14:39:46 +0800

    add_xiaoqu

---
+++ js/map-xiaoqu.js
@@ -0,0 +1,21 @@
+ function set_xiaoqu(data,map) {
+
+ 	console.log(data)
+
+ 	         
+ 	/*$.ajax({
+       url: 'https://ncov.html5.qq.com/api/getCommunity?province=%E5%8C%97%E4%BA%AC%E5%B8%82&callback=handleCallback&_=1581085944508',
+        dataType: 'json',  // 请求方式为jsonp
+        jsonp:"callback",
+        jsonpCallback:"success_jsonpCallback",  // 自定义回调函数名
+       success: (res) => {
+    	   console.log(res)
+    	  // data = JSON.parse(res);
+
+        }
+   })*/
+
+ }
+ function clear_all(){
+ 	
+ }
